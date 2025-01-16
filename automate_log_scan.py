cada1f704957db52490b095e45dc05a1de721755
Kourtney Smith <154268483+Techno-Queen-of-Tesla@users.noreply.github.com>
2025-01-16 04:04:37 +0000

    added formatting for date in send_email.txt file

--- automate_log_scan.py
+++ automate_log_scan.py
@@ -1,10 +1,11 @@
 import datetime
-from crontab import CronTab
+import crontab
 
 def main():
-    now = datetime.datetime.now()
-    with open(r"send_email.txt", "w"):
-        print(f"Cron job executed at {now}")
+    now = datetime.datetime.now() # gets the current time
+    now_formatted = now.strftime('%m/%d/%Y %H:%M:%S') # formats the time into MM/DD/YYY HH:MM:SS format
+    with open("send_email.txt", "w") as email_message:
+        email_message.writelines(f"access.log and auth.log last scanned at {now_formatted}") # writes the log entry to an external .txt file
 
 if __name__ == "__main__":
     main()
